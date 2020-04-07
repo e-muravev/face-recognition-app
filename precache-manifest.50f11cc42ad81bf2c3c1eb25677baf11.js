f4be756f15a7fb314ef813bcfa319f4e7ef1e9f9
e-muravev <e.muravev.js@gmail.com>
2020-04-07 14:08:38 +0700

    Updates

---
+++ precache-manifest.50f11cc42ad81bf2c3c1eb25677baf11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b683d32c15cf4a76bf007b07195e373d",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "eec3c90f26246e48cac6",
+    "url": "/face-recognition-app/static/css/main.a91095e7.chunk.css"
+  },
+  {
+    "revision": "ce32fa40f2ad0f262117",
+    "url": "/face-recognition-app/static/js/2.09783add.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/face-recognition-app/static/js/2.09783add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec3c90f26246e48cac6",
+    "url": "/face-recognition-app/static/js/main.2dca06d3.chunk.js"
+  },
+  {
+    "revision": "4dab0aeb0d120cab9306",
+    "url": "/face-recognition-app/static/js/runtime-main.c2e5c8a8.js"
+  },
+  {
+    "revision": "f6087f8741a50ec2ef7e10977d4f6efc",
+    "url": "/face-recognition-app/static/media/Flag_UK.f6087f87.png"
+  },
+  {
+    "revision": "4bcbb0a045f26689d224452b2f39f07b",
+    "url": "/face-recognition-app/static/media/face.4bcbb0a0.png"
+  }
+]);
